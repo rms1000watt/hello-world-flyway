0aef46856264d5c7bd015aad19e243603d400c15
Ryan Smith <rms1000watt@gmail.com>
2018-02-02 10:20:39 -0800

    Init commit

---
+++ sql/V1_1__Create_Person.sql
@@ -0,0 +1,6 @@
+CREATE TABLE Person (
+  ID   int          not null,
+  NAME varchar(100) not null,
+  
+  PRIMARY KEY(ID)
+);
---
+++ sql/V1_2__Insert_Persons.sql
@@ -0,0 +1,3 @@
+INSERT INTO Person (ID, Name) VALUES (1, 'Axel');
+INSERT INTO Person (ID, Name) VALUES (2, 'Mr. Foo');
+INSERT INTO Person (ID, Name) VALUES (3, 'Ms. Bar');
